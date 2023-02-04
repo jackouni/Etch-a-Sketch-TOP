de86b9111d50a6059c131d053bdb6165cb0e4091
Jack Sebben <jacksebben11@hotmail.com>
2023-02-04 08:11:21 -0500

    Added a function 'setGridSize(px)' that takes an argument (user's input, 'px') and sets the CSS grid dimensions for the canvas, mainContainer

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 const mainContainer = document.getElementById('main-container');
-const createDiv = document.createElement('div')
-mainContainer.setAttribute("style", 'display: grid; grid-template-rows: repeat(16, 33.75px) ; grid-template-columns: repeat(16, 33.75px) ;')
 
-function addDiv(x){
-    for (let i = 0 ; i < (x * x) ; i++ ){
+
+
+function setGridSize(px){// Sets the CSS Grid size/dimensions for the canvas, mainContainer.
+    mainContainer.setAttribute('style', `grid-template-rows: repeat(${px}px, auto) ; grid-template-columns: repeat(${px}px, auto) ;`)
+}
+
+function addDiv(px){// Creates & adds the <div>s to fill the canvas, mainContainer.
+    for (let i = 0 ; i < (px * px) ; i++ ){
     let createDiv = document.createElement('div')
     createDiv.style.backgroundColor = 'blue'
+    createDiv.classList.add('canvas-px')
     createDiv.style.width = 'auto'
     createDiv.style.height = 'auto'
     mainContainer.appendChild(createDiv);
     }
 }
 
-/* const gridSize = A variable that determines the square dimension of container 
-(16x16 to start) */
 
 /* function determinePxSize() {
     A function that'll take our gridSize and determine the dimensions for each <div>
@@ -22,13 +25,5 @@ function addDiv(x){
         On a 16x16 grid with our canvas being 540x540px each <div> would have to be...
         
 } */
-
-/*function addDiv(){
-    for (let i=0; i <= 32 ; i++){
-    const newDiv = document.createElement('div');
-    newDiv.setAttribute('style', ' flex: 1; border:solid red 2px; width: 33.75px; height: 33.75px;')
-    mainContainer.appendChild(newDiv);
-    console.log('add') }
-}*/
 
 addDiv(16)
