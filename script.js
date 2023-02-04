af6b46e376f9d4191e9fa58e60f34ab70e9d1057
Jack Sebben <jacksebben11@hotmail.com>
2023-02-04 08:53:54 -0500

    Forgot to commit this change: Changed the var name for 'mainContainer' to 'canvas' to make things more readable.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const mainContainer = document.getElementById('main-container');
+const canvas = document.getElementById('main-container');
 let px = 16
 
 function runProgram(){
@@ -13,18 +13,20 @@ function runProgram(){
     });
 }
 
-function setGridSize(px){// Sets the CSS Grid size/dimensions for the canvas, mainContainer.
-    mainContainer.setAttribute('style', `grid-template-rows: repeat(${px}, auto); grid-template-columns: repeat(${px}, auto);`)
+
+
+function setGridSize(px){// Sets the CSS Grid size/dimensions for the canvas, canvas.
+    canvas.setAttribute('style', `grid-template-rows: repeat(${px}, auto); grid-template-columns: repeat(${px}, auto);`)
 }
 
-function addDiv(px){// Creates & adds the <div>s to fill the canvas, mainContainer.
+function addDiv(px){// Creates & adds the <div>s to fill the canvas, canvas.
     for (let i = 0 ; i < (px * px) ; i++ ){
     let createDiv = document.createElement('div')
     createDiv.style.backgroundColor = 'blue'
     createDiv.classList.add('canvas-px')
     createDiv.style.width = 'auto'
     createDiv.style.height = 'auto'
-    mainContainer.appendChild(createDiv);
+    canvas.appendChild(createDiv);
     }
 }
 
