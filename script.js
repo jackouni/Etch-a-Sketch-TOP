206eb69821c5ce995ea68cf55122401ddf93d2f9
Jack Sebben <jacksebben11@hotmail.com>
2023-02-05 20:51:58 -0500

    Revised the code so that the resetGrid and removeDiv functions are more readable. The resetGrid function sets the grid to the new adjusted value and removeDivremoves the pre-existing <div>'s. After that the setGrid and addDiv functions run. This sequence is triggered by an event listener on the slider.

--- script.js
+++ script.js
@@ -1,47 +1,65 @@
 const canvas = document.getElementById('main-container');
-let slider = document.getElementById('grid-range');
-let output = document.getElementById('value');
-let px = slider.value;
+const slider = document.getElementById('grid-range');
+const output = document.getElementById('value');
+let canvasPxs = document.querySelectorAll('.canvas-px');
 
-output.textContent = slider.value ;
-
-slider.oninput = function() {
-    output.textContent = this.value ;
-}
 
 function runProgram(){
-    setGridSize(px)
-    addDiv(px)
+    console.log('runProgram() invoked')
 
-    const canvasPxs = document.querySelectorAll('.canvas-px');
-    canvasPxs.forEach((canvasPx) => {
-    canvasPx.addEventListener('mouseover', () => {
-        canvasPx.setAttribute('style', 'background-color: red;')
-    });
-    });
+    
+    canvasPxs.forEach((canvasPx) => 
+    canvasPx.addEventListener('mousedown', () => {
+        canvasPx.setAttribute('style', 'background-color: red;');
+    } ));
+
+    output.textContent = slider.value ;
+    slider.oninput = function() {
+        output.textContent = this.value ;
+    }
+
+
+    
+    slider.addEventListener('mouseup', () => {
+        resetGrid();
+        removeDiv();
+        setGrid(slider.value);
+        addDiv(slider.value);
+        console.log('slider "mouseup" event')
+    })
+    
 }
 
-function setGridSize(px){ // Sets the CSS Grid size/dimensions for the canvas, canvas.
+function setGrid(px){ // Sets the CSS Grid size/dimensions for the canvas, canvas.
     canvas.setAttribute('style', `grid-template-rows: repeat(${px}, auto); grid-template-columns: repeat(${px}, auto);`)
+    return console.log('setGrid() invoked');
 }
 
 function addDiv(px){// Creates & adds the <div>s to fill the canvas, canvas.
     for (let i = 0 ; i < (px * px) ; i++ ){
     let createDiv = document.createElement('div')
-    createDiv.style.backgroundColor = 'blue'
+    createDiv.style.backgroundColor = 'white'
     createDiv.classList.add('canvas-px')
     createDiv.style.width = 'auto'
     createDiv.style.height = 'auto'
     canvas.appendChild(createDiv);
     }
+    return console.log('addDiv() invoked');
 }
 
-function resetGrid(){ // Resets the grid by removing all created pxs
+function resetGrid(){ // Resets the CSS grid by changing the 'style' properties to their original.
+    canvas.setAttribute('style', `grid-template-rows: repeat(8, auto); grid-template-columns: repeat(8, auto);`)
+    return console.log('resetGrid() invoked')
+}
+
+function removeDiv(px){ // Resets the grid by removing all created pxs
     const canvasPxs = document.querySelectorAll('.canvas-px');
     canvasPxs.forEach((canvasPx) => {
-        canvas.removeChild(canvasPx)
+        canvasPx.remove();
     });     
+    return console.log('removeDiv() invoked')
 }
+
 
 
 runProgram()
